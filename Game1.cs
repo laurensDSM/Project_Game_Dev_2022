e5b722f8b4bc2513b0303719d59763d1a1d74275
Laurens <Laurens.desmedt@student.ap.be>
2022-12-08 08:44:45 +0100

    2 vierkantjes getekend

--- Game1.cs
+++ Game1.cs
@@ -8,12 +8,19 @@ namespace Project_Game_Dev_2022
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+        Texture2D blokTexture;
+        Rectangle hero;
+        Rectangle box;
+        Vector2 positie = new Vector2(0, 0);
+        Vector2 positie2;
+
 
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
+            positie2 = new Vector2(120,100);
         }
 
         protected override void Initialize()
@@ -21,11 +28,20 @@ namespace Project_Game_Dev_2022
             // TODO: Add your initialization logic here
 
             base.Initialize();
+            hero = new Rectangle((int)positie.X, (int)positie.Y, 10 * 5, 10 * 5);
+
+            box = new Rectangle((int)positie2.X, (int)positie2.Y, 10 * 8, 10 * 8);
+
         }
 
         protected override void LoadContent()
         {
+            blokTexture = new Texture2D(GraphicsDevice, 1, 1);
+            blokTexture.SetData(new[] { Color.White });
+
+          
             _spriteBatch = new SpriteBatch(GraphicsDevice);
+
 
             // TODO: use this.Content to load your game content here
         }
@@ -43,8 +59,13 @@ namespace Project_Game_Dev_2022
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(blokTexture, hero, Color.Red);
+            _spriteBatch.Draw(blokTexture, box, Color.Green);
+
 
             // TODO: Add your drawing code here
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
