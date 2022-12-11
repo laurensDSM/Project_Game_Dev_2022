532bf37f6fb3de3a3fae6285a4b25c51fea66f97
Laurens <Laurens.desmedt@student.ap.be>
2022-12-11 12:01:14 +0100

    aanmaken van interface IGameObject

--- Hero.cs
+++ Hero.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Project_Game_Dev_2022.interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Project_Game_Dev_2022
 {
-    public class Hero
+    public class Hero:IGameObject
     {
         public void Update()
         {
---
+++ interfaces/IGameObject.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Game_Dev_2022.interfaces
+{
+    interface IGameObject
+    {
+        void Update();
+        void Draw();
+    }
+}
