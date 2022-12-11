c0de159e42818fe19c4f37a95835205b864c9c12
Laurens <Laurens.desmedt@student.ap.be>
2022-12-11 11:58:11 +0100

    class hero aanmaken

---
+++ Hero.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Game_Dev_2022
+{
+    public class Hero
+    {
+        public void Update()
+        {
+
+        }
+        public void Draw()
+        {
+
+        }
+    }
+
+
+}
